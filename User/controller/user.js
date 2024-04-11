4f31fa44088b0061e2e576c75822605669f9607d
Robnelito <christianorak@gmail.com>
2024-04-11 21:11:45 +0300

    register user

---
+++ User/controller/user.js
@@ -0,0 +1,50 @@
+const {PrismaClient} = require('@prisma/client')
+const {v4: uuidv4} = require('uuid');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const salt = bcrypt.genSaltSync(saltRounds);
+
+const prisma = new PrismaClient()
+const root = async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+  return res.json(allUsers);
+}
+
+const isExistingUser = async (user_email) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      user_email: user_email
+    }
+  })
+
+  if (user) return true;
+  else return false
+}
+const register = async (req, res) => {
+  if (!req.body.user_firstname || !req.body.user_email) {
+    res.json({tag: "error", message: "firstname or email is empty"})
+    return
+  }
+
+  if (await isExistingUser(req.body.user_email)) {
+    res.json({tag: "error", message: "user already exist"})
+    return
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      user_id: uuidv4(),
+      user_firstname: req.body.user_firstname,
+      user_lastname: req.body.user_lastname,
+      user_email: req.body.user_email,
+      user_password: bcrypt.hashSync(req.body.user_password, salt)
+    },
+  })
+
+  res.json(user)
+}
+
+module.exports = {
+  root,
+  register
+};
--- User/index.js
+++ User/index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const PORT = 3001
+const userRoute = require('./controller/user')
 
-app.get('/', (req, res) => {
-  res.send("user")
-})
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
+
+app.use(bodyParser.json());
+
+app.get('/', userRoute.root)
+app.post('/', userRoute.register)
 
 app.listen(PORT, () => {
-  console.log(`user listen on port ${PORT}`);
+    console.log(`user listen on port ${PORT}`);
 })
