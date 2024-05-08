8adba7aa3b00eba0c8cd798477c80517d71daa34
Robnelito <christianorak@gmail.com>
2024-05-08 20:53:30 +0300

    login form police

--- Frontend/src/pages/user/login.jsx
+++ Frontend/src/pages/user/login.jsx
@@ -1,26 +1,43 @@
+import {useState} from "react";
+
 function Login() {
+  const [seePassword, setSeePassword] = useState(false)
+  const [userInfo, setUserInfo] = useState({identifiant: '', password: ''})
+
+  const connect = (e) => {
+    e.preventDefault()
+    alert('coco')
+  }
+
   return (
     <div className={'h-dvh w-dvw flex justify-center place-items-center'}>
       <div className={'p-4 border rounded-md space-y-2'}>
         <div>
-          <h1 className={'font-bold text-2xl'}>Connexion</h1>
-          <p className={'text-sm text-gray-600 font-thin'}>Connexion à l'application de gestion de virement
+          <h1 className={'font-black text-2xl'}>Connexion</h1>
+          <p className={'text-sm text-gray-600 font-light'}>Connexion à l&apos;application de gestion de virement
             bancaire.</p>
           <hr/>
         </div>
-        <form className={' space-y-2'}>
+        <form onSubmit={connect} className={' space-y-2'}>
           <div className={''}>
             <label htmlFor="identifiant">Identifiant ou email</label>
-            <input type="text" id={'identifiant'} name={'identifiant'}
+            <input onChange={(e) => {
+              setUserInfo({...userInfo, identifiant: e.target.value})
+            }} type="text"
+                   id={'identifiant'} name={'identifiant'}
                    className={'block border w-full rounded-md p-2'}/>
           </div>
           <div className={''}>
             <label htmlFor="password">Mot de passe</label>
-            <input type="text" id={'password'} name={'password'}
+            <input onChange={(e) => {
+              setUserInfo({...userInfo, password: e.target.value})
+            }} type={seePassword ? "text" : "password"} id={'password'} name={'password'}
                    className={'block border w-full rounded-md p-2'}/>
           </div>
           <div className={'space-x-2'}>
-            <input type="checkbox" id={'seePassword'} name={'seePassword'}/>
+            <input onChange={(e) => {
+              setSeePassword(e.target.checked)
+            }} type="checkbox" id={'seePassword'} name={'seePassword'}/>
             <label htmlFor="seePassword">Mot de passe</label>
           </div>
           <div className={'flex justify-end'}>
