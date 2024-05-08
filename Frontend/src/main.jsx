cbe517ae83d2e03712c49f4861df57256ef840fd
Hasina-Tiana <hasinatianarah@gmail.com>
2024-05-08 19:15:30 +0300

    Ajout du route client

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,12 +6,17 @@ import {
 } from "react-router-dom";
 import './index.css'
 import Login from "./pages/user/login.jsx";
+import Client from "./pages/client/Index.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login/>,
   },
+  {
+    path: "/client",
+    element: <Client/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
