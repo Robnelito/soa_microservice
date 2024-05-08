c999505291acdd2185e930aad4319e19f0e3264d
Hasina-Tiana <hasinatianarah@gmail.com>
2024-05-08 16:49:39 +0300

    création CRUD pour le client

--- Client/index.js
+++ Client/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const PORT = 3002
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const clients = require('./controller/client');
 
-app.get('/', (req, res) => {
-  res.send("client")
-})
+app.use(cors({
+  origin: '*',
+  methods: 'GET, POST, PUT, DELETE',
+  credentials: true,
+}));
+
+app.use(bodyParser.json());
+
+app.post('/clients', clients.createClient);
+app.get('/clients', clients.getClients);
+app.get('/client/:idClient', clients.getClientById);
+app.put('/client/:idClient', clients.updateClient);
+app.delete('/client/:idClient', clients.deleteClient);
 
 app.listen(PORT, () => {
   console.log(`client listen on port ${PORT}`);
