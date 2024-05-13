686e9db5f12f55aad8a52ec6cb0ac06a6224dc12
Andokely <andoratovo12@gmail.com>
2024-05-13 20:14:10 +0300

    module payment

--- payment/index.js
+++ payment/index.js
@@ -3,13 +3,20 @@ const app = express()
 const bodyParser = require('body-parser')
 const PORT = 3003
 const payment = require('./controller/payment')
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-  res.send("payment")
-})
+app.use(cors({
+  origin: '*',
+  methods: 'GET, POST, PUT, DELETE',
+  credentials: true,
+}));
 
 app.use(bodyParser.json());
-app.post('/', payment.createPayment);
+app.post('/', payment.getPayment);
+app.post('/updateSoldeClient', payment.updateSoldeClient);
+
+app.get('/:paymentId', payment.getPaymentById);
+app.delete('/:paymentId', payment.deletePayment);
 
 
 app.listen(PORT, () => {
